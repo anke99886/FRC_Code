e79d2fc87b6a00e3c413257062a700b1e0cdbbe6
anke99886 <anke.beukes@education.nsw.gov.au>
2018-02-04 10:50:08 +1100

    Update BoxLifter.java

--- FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
@@ -6,9 +6,9 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType; 
-
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 
 
