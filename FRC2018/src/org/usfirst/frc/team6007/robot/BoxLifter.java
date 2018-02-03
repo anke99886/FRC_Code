3a2cd99931cdcc333df50cd6a1aa70889324ba23
anke99886 <anke.beukes@education.nsw.gov.au>
2018-02-04 10:29:14 +1100

    Update BoxLifter.java
    take the // or /* away if code is right

--- FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive
 import edu.wpi.first.wpilibj.RobotDrive.MotorType; 
 
 
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 
 public class BoxLifter(){
  
-  RobotDrive lifterBase;
+  DifferentailDrive lifterBase;
   
   //constructor
   
@@ -23,9 +23,17 @@ public class BoxLifter(){
   }
  // maybe code(function) to return the arm for the low delivery
  
- 
+ /*	Spark frontLeftMotor = new Spark(FRONT_LEFT_MOTOR_ID);
+		Spark rearLeftMotor = new Spark(REAR_LEFT_MOTOR_ID);
+		Spark frontRightMotor = new Spark(FRONT_RIGHT_MOTOR_ID);
+		Spark rearRightMotor = new Spark(REAR_RIGHT_MOTOR_ID);*/
+
  // maybe code (function) for the switch delivery
  
+ /*	public void autonomous(){
+		driveBase.setSafetyEnabled(false);
+  
+ //	driverStick = new Joystick(0);
  
   // maybe code (function) for the scales delivery
 }
